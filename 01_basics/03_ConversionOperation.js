028aaeff3e72cc49c4f0d5affbdff48f67ea1dc6
pradeepadlakondawar <94275183+pradeepadlakondawar@users.noreply.github.com>
2023-10-18 10:56:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,37 @@
+let score ="33";
+let score1 ="33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+let valueInNumber1 = Number(score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+
+let isLoggedIn ="pro";
+
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 =>ture
+//0 => false
+//"" => false
+// "pradeep" =>ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
