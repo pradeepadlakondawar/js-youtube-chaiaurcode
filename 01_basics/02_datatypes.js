2ab0f05dddb7f1f18f2517679c417fb9b6235f66
pradeepadlakondawar <94275183+pradeepadlakondawar@users.noreply.github.com>
2023-10-18 10:31:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use Strict"; //treats all code as a newer version of JS code
+
+// alert("hello") // we are using node js not browser
+
+//always focus on the readablity
+
+//tc39.es   & MDN for refrence
+
+
+//Premitive datatypes
+// number
+// BigInt  - generally we dont use this, in case a bignumber is there at that we use it
+//string => ""
+//Boolean => TRUE or FALSE
+//null => standalone value  // representation of empty 
+//undefined => when value is not defined
+//symbol ==> generaly used to find uniqueness , it is used in reactjs figma
+
+
+//object
+
+console.log(typeof "hitesh");  //string
+console.log(typeof null);  //object
