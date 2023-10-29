7b75a7b6bc481a9c738b766012b7d70936282a8c
pradeepadlakondawar <94275183+pradeepadlakondawar@users.noreply.github.com>
2023-10-29 18:03:41 +0000

    date_time

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -23,7 +23,17 @@ console.log(myCreatedDate.toLocaleString());
 ///////----------TIMESTAMP---------
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000)) //converting into seconds interview que
 
+let newDate= new Date();
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
 
+newDate.toLocaleString('default',{
+    weekday: "long",  //ctrl+space gives you suggestions
+    
+})
